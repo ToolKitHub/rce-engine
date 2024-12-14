534570a5709e0bff2c10296f9358bcce73004a6b
xosnrdev <successxodev@gmail.com>
2024-12-14 12:39:54 -0500

    refactor: Update ServiceInfo struct for improved serialization
    - Changed fields in ServiceInfo from static strings to owned Strings for better memory management.
    - Updated VERSION to use env! macro for direct access to the package version.
    - Adjusted the instantiation of ServiceInfo to reflect the new types.

--- src/rce_engine/api/root.rs
+++ src/rce_engine/api/root.rs
@@ -1,19 +1,22 @@
+use serde::Serialize;
+
 use crate::rce_engine::api;
 
-const VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
-#[derive(Debug, serde::Serialize)]
+#[derive(Debug, Serialize)]
 struct ServiceInfo {
-    name: &'static str,
-    version: &'static str,
-    description: &'static str,
+    name: String,
+    version: String,
+    description: String,
 }
 
 pub fn handle() -> Result<api::SuccessResponse, api::ErrorResponse> {
     let service_info = ServiceInfo {
-        name: "rce-engine",
-        version: VERSION.unwrap_or("unknown"),
-        description: "Docker-based engine for executing untrusted code in isolated containers.",
+        name: "rce-engine".to_string(),
+        version: VERSION.to_string(),
+        description: "Docker-based engine for executing untrusted code in isolated containers."
+            .to_string(),
     };
 
     api::prepare_json_response(&service_info, api::JsonFormat::Pretty)
